917dded53809e9cda0ad1bad7f45ef2a929b1548
pixelforge21 <serverdestroyerr@gmail.com>
2025-08-10 11:20:06 +0530

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,12 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from authlib.integrations.flask_client import OAuth
 
 db = SQLAlchemy()
+migrate = Migrate()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 mail = Mail()
 oauth = OAuth()
+
+# login view
+login_manager.login_view = "auth_bp.login"
+login_manager.login_message_category = "info"
+
 
