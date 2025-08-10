5ca9b9a902c43da15aa51b8a34c3c56634f257af
pixelforge21 <serverdestroyerr@gmail.com>
2025-08-10 10:57:12 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+from .extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    mobile = db.Column(db.String(20), nullable=False)
+    student_class = db.Column(db.String(20), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
 
