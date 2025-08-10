4aec86aac335a46f3cfa3df19f489502ac8637e4
pixelforge21 <serverdestroyerr@gmail.com>
2025-08-10 11:21:37 +0530

    Update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,1 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, NumberRange
+
+class RegisterForm(FlaskForm):
+    name = StringField("Full name", validators=[DataRequired(), Length(min=2, max=120)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Sign Up")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+class EnrollmentForm(FlaskForm):
+    name = StringField("Full name", validators=[DataRequired(), Length(min=2, max=120)])
+    mobile = StringField("Mobile", validators=[DataRequired(), Length(min=6, max=20)])
+    student_class = StringField("Class", validators=[DataRequired(), Length(min=1, max=30)])
+    age = IntegerField("Age", validators=[DataRequired(), NumberRange(min=5, max=120)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Enroll")
 
