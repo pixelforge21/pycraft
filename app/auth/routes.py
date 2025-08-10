fc1faa8ba94aa63d81a4ba34f9496c124bf02ab8
pixelforge21 <serverdestroyerr@gmail.com>
2025-08-10 11:03:15 +0530

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,1 +1,66 @@
+from flask import render_template, redirect, url_for, flash, request, session
+from flask_login import login_user, logout_user, current_user, login_required
+from . import auth
+from .forms import RegisterForm, LoginForm
+from ..models import User
+from ..extensions import db, bcrypt, oauth, login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('main.home'))
+        flash("Invalid credentials", "danger")
+    return render_template('auth/login.html', form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(name=form.name.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('main.home'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/google')
+def google_login():
+    redirect_uri = url_for('auth.google_callback', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+@auth.route('/google/callback')
+def google_callback():
+    token = oauth.google.authorize_access_token()
+    user_info = oauth.google.userinfo()
+    google_id = user_info['sub']
+    email = user_info['email']
+    name = user_info.get('name', email.split('@')[0])
+
+    user = User.query.filter_by(google_id=google_id).first()
+    if not user:
+        user = User(email=email, name=name, google_id=google_id)
+        db.session.add(user)
+        db.session.commit()
+
+    login_user(user)
+    return redirect(url_for('main.home'))
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
 
