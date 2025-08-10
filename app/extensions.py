358ea954e4167069a4e4a8ba558594b3be3609c3
pixelforge21 <serverdestroyerr@gmail.com>
2025-08-10 11:00:58 +0530

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+from authlib.integrations.flask_client import OAuth
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+mail = Mail()
+oauth = OAuth()
 
