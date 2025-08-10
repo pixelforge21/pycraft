568a41e9372f3715a68390c33aaedbe5644ab0f9
pixelforge21 <serverdestroyerr@gmail.com>
2025-08-10 10:56:33 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from .config import Config
+from .extensions import db
+import os
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Init extensions
+    db.init_app(app)
+
+    # Register blueprints
+    from .main.routes import main
+    app.register_blueprint(main)
+
+    return app
 
