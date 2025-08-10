077245c8d352f691d177ee42538c698688769b08
pixelforge21 <serverdestroyerr@gmail.com>
2025-08-10 11:01:53 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
 from .extensions import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200))  # Nullable for Google accounts
+    name = db.Column(db.String(100))
+    google_id = db.Column(db.String(200), unique=True, nullable=True)
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
